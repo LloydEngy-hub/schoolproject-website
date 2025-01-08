cc232f5b302b2a7bad134862f4c8ffa16f88e5da
Clifford-Joshua <155918426+Clifford-Joshua@users.noreply.github.com>
2025-01-08 18:54:02 +0100

    navbar complete

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("home page");
+// ===============================================
+// functions
+// open function
+const OpenFunction = (div, classes) => {
+  div.classList.add(classes);
+};
+
+// close function
+const CloseFunction = (div, classes) => {
+  div.classList.remove(classes);
+};
+
+const ToggleFunction = (div, classes) => {
+  div.classList.toggle(classes);
+};
+
+// ===================================================
+// nav elements
+const nav_toggle_btn = document.querySelector(".nav_toggle");
+const nav_container = document.querySelector(".nav_container");
+const mobile_nav = document.querySelector(".mobile_nav");
+const Mobile_nav_container = document.querySelector(".mobile_nav_container");
+
+// Mobile nav functionality
+nav_toggle_btn.addEventListener("click", (e) => {
+  const currentIcon = e.target;
+
+  // change toggle icon
+  ToggleFunction(nav_toggle_btn, "toggle_nav");
+
+  // get mobile nav container height
+  const height = Mobile_nav_container.getBoundingClientRect().height;
+
+  const navInitialHeight = mobile_nav.getBoundingClientRect().height;
+
+  if (currentIcon.classList.contains("fa-bars")) {
+    nav_container.style.height = `calc(${height}px + 1rem)`;
+  } else {
+    nav_container.style.height = `calc(${navInitialHeight}px + 1rem)`;
+    console.log(navInitialHeight);
+  }
+});
